f599374337c038ba85663b5ebf15412a86d687e7
pipejava <ingandres0203@gmail.com>
2022-11-17 14:07:17 -0500

    11172022

---
+++ js/conceptos.js
@@ -0,0 +1,17 @@
+// CONCEPTOS BASICO EN JAVASCRIPT   
+/* Declarar variables en java script:
+1. var (forma estandar, nevgadores activos)
+2. let (variable que puede cambiar)
+3. const(variable que mantiene constante)
+
+todo texto va entre comillas
+*/ 
+
+var nombre = "Andres"; //variable tipo cadena o texto
+let edad = 45; //variable tipo entero
+const iva = 0.19 // variable de tipo flotante
+
+// datos de salida o imprimir un dato en la pagina web
+document.write(nombre);
+// console.log en la consola del navegador o devtools
+console.log(nombre);
