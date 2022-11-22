ab4271685a60b9f82189a379e6a0ba989494ab7a
pipejava <ingandres0203@gmail.com>
2022-11-22 14:11:00 -0500

    11222022

---
+++ js/Factura.js
@@ -0,0 +1,7 @@
+//crear una funcion
+function calcular(){
+    //definir variables para el iva, precio y cantidades
+    const iva = 0.19;
+    let precio = document.simuladorFactura.precio.value; 
+
+}
